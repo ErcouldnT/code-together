5223dad7be7eb6b8f396559a1f0219d7911b54ab
ErcouldnT <erkut.tekoglu@gmail.com>
2021-06-20 23:10:43 +0300

    Mongo timestamps

--- model/Document.js
+++ model/Document.js
@@ -3,6 +3,8 @@ const { Schema, model } = require("mongoose");
 const Document = new Schema({
   _id: String,
   data: Object,
+}, {
+  timestamps: true,
 });
 
 module.exports = model("Document", Document);
